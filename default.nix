f59e475e85e01b0ba7709650d7176d557dcea7a5
izzy <me@insrt.uk>
2025-08-30 11:55:23 +0200

    docs: update the about page

--- default.nix
+++ default.nix
@@ -5,12 +5,13 @@
 pkgs.mkShell rec {
   buildInputs = [
     # Nix formatter
-    pkgs.nixfmt-rfc-style    
+    pkgs.nixfmt-rfc-style
 
     # Tools
     pkgs.git
 
     # Node
+    pkgs.deno
     pkgs.nodejs
     pkgs.nodejs.pkgs.pnpm
   ];
