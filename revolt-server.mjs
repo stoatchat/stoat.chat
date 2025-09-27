6619ee9ffb605aace24ec951b07cbd8d080c74f6
Infi <infi@infi.sh>
2025-09-27 20:51:50 +0200

    fix: 404 page not appearing properly

--- revolt-server.mjs
+++ revolt-server.mjs
@@ -9,6 +9,10 @@ app.set("x-powered-by", false)
 const base = "/"
 app.use(base, express.static("dist/client/"))
 app.use(ssrHandler)
+app.use((req, res, next) => {
+    req.url = "/404"
+    ssrHandler(req, res, next)
+})
 
 const port = Number(process.env.PORT) || 8080
 
