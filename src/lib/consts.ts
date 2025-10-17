4524d6776374adfe2349de24a83b3432d28176be
izzy <me@insrt.uk>
2025-10-17 14:58:38 +0100

    docs: update download links

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,12 +1,22 @@
 export default class Consts {
     static readonly MSSTORE_URL =
-        "ms-windows-store://pdp/?ProductId=9nmtvmrj7n1k" as const
+        "ms-windows-store://pdp/?ProductId=9nmtvmrj7n1k" as const;
     static readonly PLAYSTORE_URL =
-        "https://play.google.com/store/apps/details?id=chat.revolt" as const
+        "https://play.google.com/store/apps/details?id=chat.revolt" as const;
     static readonly FLATHUB_URL =
-        "https://flathub.org/apps/chat.revolt.RevoltDesktop" as const
+        "https://flathub.org/apps/chat.revolt.RevoltDesktop" as const;
     static readonly TESTFLIGHT_URL =
-        "https://testflight.apple.com/join/mGSCJe13" as const
+        "https://testflight.apple.com/join/mGSCJe13" as const;
     static readonly GITHUB_URL =
-        "https://github.com/revoltchat/desktop/releases/" as const
+        "https://github.com/stoatchat/for-desktop/releases/" as const;
+    static readonly WINDOWS_BUILD_URL =
+        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/stoat-desktop-setup.exe" as const;
+    static readonly MACOS_ARM64_BUILD_URL =
+        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-darwin-arm64-1.1.11.zip" as const;
+    static readonly MACOS_X64_BUILD_URL =
+        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-darwin-x64-1.1.11.zip" as const;
+    static readonly LINUX_UNPACKED_ARM64_BUILD_URL =
+        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-linux-arm64-1.1.11.zip" as const;
+    static readonly LINUX_UNPACKED_X64_BUILD_URL =
+        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-linux-x64-1.1.11.zip" as const;
 }
