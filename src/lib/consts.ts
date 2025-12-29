4fdd30367f4a1a8ded39b2130af6b18d620d9bd1
Paul Makles <me@insrt.uk>
2025-12-29 19:14:26 +0100

    chore: bump desktop release (#56)

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,3 +1,5 @@
+const LATEST_BUILD = "1.1.12" as const;
+
 export default class Consts {
     static readonly MSSTORE_URL =
         "ms-windows-store://pdp/?ProductId=9nmtvmrj7n1k" as const;
@@ -10,13 +12,13 @@ export default class Consts {
     static readonly GITHUB_URL =
         "https://github.com/stoatchat/for-desktop/releases/" as const;
     static readonly WINDOWS_BUILD_URL =
-        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/stoat-desktop-setup.exe" as const;
+        `https://github.com/stoatchat/for-desktop/releases/download/v${LATEST_BUILD}/stoat-desktop-setup.exe` as const;
     static readonly MACOS_ARM64_BUILD_URL =
-        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-darwin-arm64-1.1.11.zip" as const;
+        `https://github.com/stoatchat/for-desktop/releases/download/v${LATEST_BUILD}/Stoat-darwin-arm64-${LATEST_BUILD}.zip` as const;
     static readonly MACOS_X64_BUILD_URL =
-        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-darwin-x64-1.1.11.zip" as const;
+        `https://github.com/stoatchat/for-desktop/releases/download/v${LATEST_BUILD}/Stoat-darwin-x64-${LATEST_BUILD}.zip` as const;
     static readonly LINUX_UNPACKED_ARM64_BUILD_URL =
-        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-linux-arm64-1.1.11.zip" as const;
+        `https://github.com/stoatchat/for-desktop/releases/download/v${LATEST_BUILD}/Stoat-linux-arm64-${LATEST_BUILD}.zip` as const;
     static readonly LINUX_UNPACKED_X64_BUILD_URL =
-        "https://github.com/stoatchat/for-desktop/releases/download/v1.1.11/Stoat-linux-x64-1.1.11.zip" as const;
+        `https://github.com/stoatchat/for-desktop/releases/download/v${LATEST_BUILD}/Stoat-linux-x64-${LATEST_BUILD}.zip` as const;
 }
