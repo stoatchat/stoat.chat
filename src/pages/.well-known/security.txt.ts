273c5de7571c42986d7bc8b14872ffcfcd513193
infi <infi@infi.sh>
2026-09-10 17:52:41 +0200

    chore: update security.txt
    just in time for 2026
    
    Signed-off-by: infi <infi@infi.sh>

--- src/pages/.well-known/security.txt.ts
+++ src/pages/.well-known/security.txt.ts
@@ -2,13 +2,13 @@ import type { APIRoute } from "astro"
 
 export const GET: APIRoute = () => {
     return new Response(
-        `Contact: mailto:security@revolt.chat
-Contact: https://github.com/revoltchat/backend/security/advisories/new
-Contact: https://github.com/revoltchat/frontend/security/advisories/new
-Contact: https://github.com/revoltchat/android/security/advisories/new
-Contact: https://github.com/revoltchat/ios/security/advisories/new
-Policy: https://github.com/revoltchat/.github/blob/master/.github/SECURITY.md
-Expires: 2026-01-01T00:00:00Z`,
+        `Contact: mailto:security@stoat.chat
+Contact: https://github.com/stoatchat/stoatchat/security/advisories/new
+Contact: https://github.com/stoatchat/for-web/security/advisories/new
+Contact: https://github.com/stoatchat/for-android/security/advisories/new
+Contact: https://github.com/stoatchat/for-ios/security/advisories/new
+Policy: https://github.com/stoatchat/.github/blob/main/.github/SECURITY.md
+Expires: 2027-01-01T00:00:00Z`,
         {
             headers: {
                 "Content-Type": "text/plain",
